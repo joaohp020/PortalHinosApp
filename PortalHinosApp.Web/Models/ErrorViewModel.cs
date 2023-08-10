fff00c857a9da8da3aa2f7a5d8e53395646df003
João Henrique <jh36750@gmail.com>
2023-08-10 14:23:31 -0300

    Add project files.

---
+++ PortalHinosApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortalHinosApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
