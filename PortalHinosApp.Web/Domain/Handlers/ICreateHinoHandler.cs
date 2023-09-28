a1d01767e4d8b0dd19ef4dcc23ae75a23f86c7f0
João Henrique <jh36750@gmail.com>
2023-09-28 08:22:19 -0300

    implementação do CQRS

---
+++ PortalHinosApp.Web/Domain/Handlers/ICreateHinoHandler.cs
@@ -0,0 +1,10 @@
+﻿using PortalHinosApp.Web.Domain.Commands.Requests;
+using PortalHinosApp.Web.Domain.Commands.Response;
+
+namespace PortalHinosApp.Web.Domain.Handlers
+{
+    public interface ICreateHinoHandler
+    {
+        CreateHinoResponse Handle(CreateHinoRequest request);
+    }
+}
