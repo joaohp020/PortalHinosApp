69199671f6823e4a56114ce52709e8e00027aa6b
João Henrique <jh36750@gmail.com>
2023-10-30 18:46:45 -0300

    handler hino e css

---
+++ PortalHinosApp.Web/Models/Hino.cs
@@ -0,0 +1,11 @@
+﻿namespace PortalHinosApp.Web.Models
+{
+    public class Hino
+    {
+        public Guid Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Letra { get; set; }
+        public List<string>? Hinos { get; set; }
+        public DateTime? Inclusao { get; set; }
+    }
+}
